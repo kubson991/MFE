1f05c07b90243c76ac2647eab0f3ddc6293e4ca9
Sebastian Poveda <poveda1999@gmail.com>
2023-11-24 18:00:55 -0500

    nono

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,7 @@ let domain =process.env.PRODUCTION_DOMAIN
 const prodConfig={
     mode:'production',
     output:{
-        filename:'[name].[contenthash].js',
+        filename:'co.[name].[contenthash].js',
     },
     plugins:[
         new ModuleFederationPlugin({
